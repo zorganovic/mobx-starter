157ba0cb4d9605ad4b7f0fac5284adfa26ba1dae
Ryan Megidov <Ryan Megidov>
2016-10-12 23:54:24 +0200

    Refactoring #2

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,6 +1,7 @@
+import _ from 'lodash'
 import React from 'react'
 import { Match, Miss } from 'react-router'
-import { Provider, observer } from 'mobx-react'
+import { Provider } from 'mobx-react'
 import Home from './Home'
 import About from './About'
 import Login from './Account/Login'
@@ -9,26 +10,96 @@ import Register from './Account/Register'
 import NotFound from './NotFound'
 import Menu from './Common/Menu'
 
-class App extends React.Component {
-    render() {
-        return (
-        <Provider {...this.props.stores}>
-            <div>
-                <Match pattern="*" component={Menu}/>
+/*
+const MatchWithFade = ({ component, ...rest }) => {
+    function renderComponent(matchProps) {
+        console.warn(matchProps)
+        return <component {...matchProps}/>
+    }
+    return <Match {...rest} render={renderComponent}/>
+}*/
 
-                <Match exactly pattern="/" component={Home}/>
-                <Match exactly pattern="/about" component={About}/>
+class App extends React.Component {
+    componentWillMount() {
+        this.hello = 'hi'
+        if (this.props.dataPromise) {
+            //console.log('componentWillMount: ', this.props.dataPromise())
+        }
+        console.log('this.props: ', this.props.stores.todos.items)
+        /*fetchData(this.props.children, this.props.params, this.props.stores).then(async(resp) => {
+            console.log('xx+++++----', resp)
+        })*/
+    }
+
+    render() {
+        const { stores, dataPromise = _.noop } = this.props
+
+        // Wrapping with provider gives children access to stores
+        function Provide(content) {
+            return <Provider {...stores}>
+                {content}
+            </Provider>
+        }
+
+        const MatchWithFade = (props) => {
+            const { component: MatchComponent, ...rest } = props
+            return <Match {...rest} render={function(matchProps) {
+                //console.warn(matchProps, props, MatchComponent.fetchData, rest)
+                console.info('***************', MatchComponent.constructor)
+
+                /*const promise = new Promise((resolve) => {
+
+                })*/
+                /*if (MatchComponent.fetchData) {
+                    MatchComponent.fetchData(stores, matchProps.params)
+                }*/
+                return <MatchComponent {...matchProps}/>
+            }}/>
+        }
+
+        const fetchData = (MatchedComponent) => {
+            return ({ params }) => {
+                /*if (MatchedComponent.fetchData) {
+                    MatchedComponent.fetchData(stores, params).then(() => {
+                        console.log('FEEETCHHHEED: ', dataPromise())
+                    })
+                }*/
+                console.debug(MatchedComponent.fetchData)
+
+                // Server-side call
+                //if (this.updater.transaction && this.updater.transaction.renderToStaticMarkup) {
+                    Home.fetchData(stores).then(() => {
+                        dataPromise(stores)
+                        console.debug('Home.fetchData', stores.todos.items)
+                    })
+                //}
+                return <MatchedComponent/>
+            }
+        }
+
+        console.debug('App.render')
+        return Provide(
+            <div>
+                test {this.hello}
+                <Menu/>
+
+                <Match exactly pattern="/" render={fetchData(Home)}/>
+                <MatchWithFade exactly pattern="/about" component={About}/>
 
                 {/* User management */}
-                <Match exactly pattern="/login" component={Login}/>
-                <Match exactly pattern="/logout" component={Logout}/>
-                <Match exactly pattern="/register" component={Register}/>
+                <MatchWithFade exactly pattern="/login" component={Login}/>
+                <MatchWithFade exactly pattern="/logout" component={Logout}/>
+                <MatchWithFade exactly pattern="/register" component={Register}/>
 
                 <Miss component={NotFound}/>
             </div>
-        </Provider>
         )
     }
 }
+
+// Hot-reloading
+/*if (module.hot) {
+    module.hot.accept()
+}*/
 
 export default App
