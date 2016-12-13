bb9dc695714418b79577b97360c460bed03ecfc6
Ryan Megidov <ryan@megidov.com>
2016-12-13 14:40:48 +0100

    Fix serving static content

--- src/server/server.js
+++ src/server/server.js
@@ -34,7 +34,7 @@ app.use(account.routes())
 // Serve static files
 config.http.static.forEach(staticRoute => {
     logger('app:static')(staticRoute.path)
-    app.use(mount(staticRoute.url, serve(staticRoute.path)))
+    app.use(mount(staticRoute.url, convert(serve(staticRoute.path))))
 })
 
 app.use(render)
